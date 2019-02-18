965ae3aff94f67cbce46490e37803fb34e11aff5
Ali Bahraminezhad <ali.bhz@gmail.com>
2019-02-18 20:59:08 +0330

    Change version to 1.0.1

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ class KiPEOPlugin(EditBookToolPlugin, InterfaceActionBase):
     description         = 'Optimize Perssian/Arabic E-books for Amazon Kindle'
     supported_platforms = ['windows', 'osx', 'linux'] # Platforms this plugin will run on
     author              = 'Ali Bahraminezhad.' # The author of this plugin
-    version             = (1, 0, 0)   # The version number of this plugin
+    version             = (1, 0, 1)   # The version number of this plugin
     file_types          = set(['epub', 'awz3']) # The file types that this plugin will be applied to
     on_postprocess      = True # Run this plugin after conversion is complete
     minimum_calibre_version = (0, 7, 53)
